fa9a66950aad3de969931f7f215e343eb0badeb8
GuiOSousa <guilherme.olivesousa@gmail.com>
2025-09-15 21:03:02 -0300

    Repo Link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ export default function Home() {
 			<h1>Computação Gráfica</h1>
 			<p>Este site foi criado para expor os resultados das atividades da UC Computação Gráfica</p>
             <p>A Home será atualizada em breve</p>
+            <p>Respositório disponível em <a href='https://github.com/GuiOSousa/siteCG'>Site CG</a></p>
         </div>
 	);
 }
